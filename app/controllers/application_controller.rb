d9795bb5f1124b87628a7fa429a46e31de8da8b0
Tim Jedrek <tim@timjedrek.com>
2023-10-03 02:23:08 -0500

    move book download and enrollment to application controller instead of individual functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_multi_page_forms
+
+  private
+
+  def initialize_multi_page_forms
+    @enrollment = Enrollment.new
+    @book_download = BookDownload.new
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @enrollment = Enrollment.new
   end
 
   def about_us
@@ -17,55 +16,42 @@ class PagesController < ApplicationController
   # COURSES
   
   def private_pilot
-    @book_download = BookDownload.new
   end
 
   def instrument
-    @book_download = BookDownload.new
   end
 
   def commercial
-    @book_download = BookDownload.new
   end
 
   def multi
-    @book_download = BookDownload.new
   end
 
   def pilot_training
-    @book_download = BookDownload.new
   end
   
   def airline_pilot_courses
-    @book_download = BookDownload.new
   end
 
   def cfi
-    @book_download = BookDownload.new
   end
 
   def atp
-    @book_download = BookDownload.new
   end
 
   def seaplane
-    @book_download = BookDownload.new
   end
 
   def bfr
-    @book_download = BookDownload.new
   end
 
   def ipc
-    @book_download = BookDownload.new
   end
 
   def wingman
-    @book_download = BookDownload.new
   end
 
   def professional_pilot_course
-    @book_download = BookDownload.new
   end
 
   # YOUTH
@@ -77,7 +63,6 @@ class PagesController < ApplicationController
   ## PREVIOUS METHODS ##
 
   def timeline
-    @book_download = BookDownload.new
   end
 
   def stratus
