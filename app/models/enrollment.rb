e185c80a244bc38bfefc6ffce15575eaebadedd2
Tim Jedrek <tim@timjedrek.com>
2023-10-03 02:17:00 -0500

    get error message to work in lieu of not changing location column in enrollment table

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,11 +1,13 @@
 class Enrollment < ApplicationRecord
   before_validation :strip_phone_number
-  after_save :to_lacrm
+  alias_attribute :program_name, :location
+  #after_save :to_lacrm
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone, presence: true, format: { with: /\A\d{10}\z/, message: "must be a valid 10-digit phone number" }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :location, presence: true
 
   def strip_phone_number
     self.phone = phone.to_s.gsub(/[-() ]/, "")
