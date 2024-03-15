ff2164557d74f6572e90e3b81b574dccbd364c37
Tim Jedrek <tim@timjedrek.com>
2024-03-15 15:21:38 -0500

    remove seaplane

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Rails.application.routes.draw do
   get 'private-pilot-training', to: 'pages#private_pilot', as: :private_pilot
   get 'certified-flight-instructor-cfi', to: 'pages#cfi', as: :cfi
   get 'airline-transport-pilot-atp', to: 'pages#atp', as: :atp
-  get 'seaplane-pilot-training', to: 'pages#seaplane', as: :seaplane
+  #get 'seaplane-pilot-training', to: 'pages#seaplane', as: :seaplane
   get 'flight-review-bfr', to: 'pages#bfr', as: :bfr
   get 'instrument-proficiency-check-ipc', to: 'pages#ipc', as: :ipc
   get 'wingman-family-member-training', to: 'pages#wingman', as: :wingman
@@ -83,7 +83,7 @@ Rails.application.routes.draw do
   get 'cessna-172', to: 'pages#cessna172', as: :cessna172
   get 'beechcraft-duchess-multi-engine-airplane', to: 'pages#duchess', as: :duchess
   get 'redbird-flight-simulator', to: 'pages#redbird', as: :redbird
-  get 'airplane-single-engine-sea-ses', to: 'pages#seaplane_fleet', as: :seaplane_fleet
+  #get 'airplane-single-engine-sea-ses', to: 'pages#seaplane_fleet', as: :seaplane_fleet
 
   # Previous Site's Routes
   get 'aerocamp', to: 'camps#new'
