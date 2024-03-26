84d31383a530910262ad9db6343db044d84e5660
Tim Jedrek <tim@timjedrek.com>
2024-03-26 01:29:51 -0500

    run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -211,6 +211,45 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_23_034601) do
     t.text "guidance"
   end
 
+  create_table "registration_forms", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "emergency_name"
+    t.string "emergency_phone"
+    t.string "emergency_email"
+    t.string "course"
+    t.integer "hours_planned"
+    t.text "goal"
+    t.string "financed"
+    t.float "total_time"
+    t.string "certificates_held"
+    t.boolean "committed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "availability"
+    t.text "time_details"
+  end
+
+  create_table "reimbursements", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "entry_date"
+    t.string "amount"
+    t.string "preferred_payment"
+    t.string "username"
+    t.string "upload"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.string "email"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
